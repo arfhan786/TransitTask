bdd6da27bedb3a842d1e60527205f692bd55361a
Arfhan <arfhanmahmad@gmail.com>
2020-08-24 09:26:01 -0500

    Initial Commit

---
+++ CodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingChallenge
+//
+//  Created by Arfhan Ahmad on 8/24/20.
+//  Copyright © 2020 Arfhan Ahmad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
